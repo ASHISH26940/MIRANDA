a36fd6dadf1a7142a2bdc835b7a87f120741bf12
Ashish Gupta <gupta.ashish2694@gmail.com>
2023-07-04 15:07:25 +0530

    2nd

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
 const tl=gsap.timeline();
 tl.to("#page1",{
     y:"100vh",
-    scale:0.5,
+    scale:0.6,
     duration:0
 })
 tl.to("#page1",{
-    y:"100vh",
+    y:"30vh",
     duration:1,
     delay:1
 })
 
-
+tl.to("#page1",{
+    y:"0vh",
+    rotate:360,
+    scale:1,
+    duration:0.8
+})
